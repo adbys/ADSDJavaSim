4426b996c2755dc7a5946ad71ec188a1d7f0ca86
Adbys José Vascocnelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2018-06-18 10:29:14 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/main/RequestHandler.java
+++ src/main/RequestHandler.java
@@ -7,15 +7,26 @@ public class RequestHandler extends Sim_entity {
 	
 	private Sim_port bdOut;
 	private Sim_port reentranceOut;
+	private Sim_port apiManagerIn;
+	private Sim_port bdIn;
 	
 
 	public RequestHandler(String name) {
 		super(name);
 		this.bdOut = new Sim_port("BdOut");
 		this.reentranceOut = new Sim_port("ReentranceOut");
+		this.apiManagerIn = new Sim_port("ApiManagerIn");
+		this.bdIn = new Sim_port("BdIn");
 		
 		add_port(this.bdOut);
 		add_port(this.reentranceOut);
+		add_port(this.bdIn);
+		add_port(this.apiManagerIn);
+		
+	}
+	
+	@Override
+	public void body() {
 		
 	}
 
