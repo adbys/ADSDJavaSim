746f5eeeacf2004bebf8100a0433e428929aaba7
Adbys José Vascocnelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2018-06-18 09:32:15 -0300

    first commit

---
+++ src/main/RequestHandler.java
@@ -0,0 +1,12 @@
+package main;
+
+import eduni.simjava.Sim_entity;
+
+public class RequestHandler extends Sim_entity {
+
+	public RequestHandler(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
