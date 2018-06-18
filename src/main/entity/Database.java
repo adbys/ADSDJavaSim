df07d07d903dbf51756f7037a87c4add0362633e
caiovidaln <caiovidaln@gmail.com>
2018-06-18 10:35:20 -0300

    Database Ports

--- src/main/entity/Database.java
+++ src/main/entity/Database.java
@@ -17,8 +17,31 @@ public class Database extends Sim_entity {
 	private Sim_port produtoOut;
 	private Sim_port usuarioOut;	
 	
-	public Database(String arg0) {
-		super(arg0);
-	}
+	public Database(String name) {
+		super(name);
+		
+		this.estabelecimentoIn = new Sim_port("EstabelecimentoIn");
+		this.pedidoIn = new Sim_port("PedidoIn");
+		this.promocaoIn = new Sim_port("PromocaoIn");
+		this.produtoIn = new Sim_port("ProdutoIn");
+		this.usuarioIn = new Sim_port("UsuarioIn");
+		
+		this.estabelecimentoOut = new Sim_port("EstabelecimentoOut");
+		this.pedidoOut = new Sim_port("PedidoOut");
+		this.promocaoOut = new Sim_port("PromocaoOut");
+		this.produtoOut = new Sim_port("ProdutoOut");
+		this.usuarioOut = new Sim_port("UsuarioOut");
+		
+		add_port(this.estabelecimentoIn);
+		add_port(this.pedidoIn);
+		add_port(this.promocaoIn);
+		add_port(this.produtoIn);
+		add_port(this.usuarioIn);
 
+		add_port(this.estabelecimentoOut);
+		add_port(this.pedidoOut);
+		add_port(this.promocaoOut);
+		add_port(this.produtoOut);
+		add_port(this.usuarioOut);	
+	}
 }
