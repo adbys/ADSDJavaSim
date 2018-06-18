e651f61985059e877307d744a219a1ead7a65d0a
Adbys José Vascocnelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2018-06-18 10:38:58 -0300

    portas para o api manager

--- src/main/RequestHandler.java
+++ src/main/RequestHandler.java
@@ -6,20 +6,21 @@ import eduni.simjava.Sim_port;
 public class RequestHandler extends Sim_entity {
 	
 	private Sim_port bdOut;
-	private Sim_port reentranceOut;
+	private Sim_port reentradaOut;
 	private Sim_port apiManagerIn;
 	private Sim_port bdIn;
 	
 
 	public RequestHandler(String name) {
 		super(name);
+		
 		this.bdOut = new Sim_port("BdOut");
-		this.reentranceOut = new Sim_port("ReentranceOut");
+		this.reentradaOut = new Sim_port("ReentradaOut");
 		this.apiManagerIn = new Sim_port("ApiManagerIn");
 		this.bdIn = new Sim_port("BdIn");
 		
 		add_port(this.bdOut);
-		add_port(this.reentranceOut);
+		add_port(this.reentradaOut);
 		add_port(this.bdIn);
 		add_port(this.apiManagerIn);
 		
--- src/main/entity/ApiManager.java
+++ src/main/entity/ApiManager.java
@@ -22,8 +22,40 @@ public class ApiManager extends Sim_entity {
 	private Sim_port request;
 	private Sim_port response;
 	
-	public ApiManager(String arg0) {
-		super(arg0);
+	public ApiManager(String name) {
+		super(name);
+		
+		this.estabelecimentoIn = new Sim_port("EstabelecimentoIn");
+		this.estabelecimentoOut = new Sim_port("EstabelecimentoOut");
+		
+		this.pedidoIn = new Sim_port("PedidoIn");
+		this.pedidoOut = new Sim_port("PedidoOut");
+		
+		this.promocaoIn = new Sim_port("PromocaoIn");
+		this.promocaoOut = new Sim_port("PromocaoOut");
+		
+		this.produtoIn = new Sim_port("ProdutoIn");
+		this.produtoOut = new Sim_port("ProdutoOut");
+		
+		this.usuarioIn = new Sim_port("UsuarioIn");
+		this.usuarioOut = new Sim_port("UsuarioOut");
+		
+		this.request = new Sim_port("Request");
+		this.response = new Sim_port("Response");
+		
+		add_port(this.estabelecimentoIn);
+		add_port(this.estabelecimentoOut);
+		add_port(this.pedidoIn);
+		add_port(this.pedidoOut);
+		add_port(this.promocaoIn);
+		add_port(this.promocaoOut);
+		add_port(this.produtoIn);
+		add_port(this.produtoOut);
+		add_port(this.usuarioIn);
+		add_port(this.usuarioOut);
+		add_port(this.request);
+		add_port(this.response);
+		
 	}
 
 }
